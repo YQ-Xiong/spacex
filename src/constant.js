33f17fbed7d4fbaf12758703cec2d2b6c1e38601
Yingqiao Xiong <yingqiaoxiong@Yingqiaos-MBP.local>
2020-08-22 14:05:11 -0400

    modify gitignore

--- src/constant.js
+++
@@ -1,7 +0,0 @@
-export const SAT_BASE_URL = "https://www.n2yo.com/rest/v1/satellite";
-
-export const SAT_API_KEY = "KR767S-ZBP3DR-S7KL57-4J1G";
-
-export const STARLINK_CATEGORY = "52"
-
-export const NEARBY_SATELLITE = `${SAT_BASE_URL}/above`;
